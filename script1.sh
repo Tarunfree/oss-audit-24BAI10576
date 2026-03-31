22598d3b12f59823a79d555b79e68a86389608d8
Tarunfree <148556944+Tarunfree@users.noreply.github.com>
2026-03-31 19:44:29 +0530

    Update script1.sh for improved output formatting

--- script1.sh
+++ script1.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 # Script 1: System Identity Report
-# Name: Tarun Yadav
+# Student Name: Tarun Yadav
 # Roll Number: 24BAI10576
 # Chosen Software: Git
 
+#  Variables 
 STUDENT_NAME="Tarun Yadav"
+ROLL_NO="24BAI10576"
 SOFTWARE="Git Version Control System"
 
 KERNEL=$(uname -r)
 USER_NAME=$(whoami)
 UPTIME=$(uptime -p)
 DATE=$(date)
+HOME_DIR=$HOME
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2)
 
-echo "======================================"
-echo "Open Source Audit - System Identity"
-echo "======================================"
+#   Output 
+echo "=========================================="
+echo "   Open Source Software Audit Project"
+echo "=========================================="
 
 echo "Student Name: $STUDENT_NAME"
+echo "Roll Number : $ROLL_NO"
 echo "Chosen Software: $SOFTWARE"
 
 echo ""
-echo "Kernel Version: $KERNEL"
-echo "Current User: $USER_NAME"
-echo "System Uptime: $UPTIME"
-echo "Current Date: $DATE"
+echo "Linux Distribution: $DISTRO"
+echo "Kernel Version    : $KERNEL"
+echo "Current User      : $USER_NAME"
+echo "Home Directory    : $HOME_DIR"
+echo "System Uptime     : $UPTIME"
+echo "Current Date/Time : $DATE"
 
 echo ""
-echo "This Linux system is open source and released under the GPL license."
+echo "This Linux system is open source and released under the GPL License."
